f22e8e213a06568b057d46665cdc8ab93edcd52b
Torah <95339155+Termng@users.noreply.github.com>
2023-12-06 14:57:46 +0100

    Update SuperQuality.jsx

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -8,7 +8,7 @@ const SuperQuality = () => {
     className="flex justify-center items-center max-lg:flex-col gap-10 w-full max-container flex-wrap"
     >
       <div className="flex flex-1 flex-col items-start">
-          <h3 className="font-palanquin capitalize text-4xl max-sm:text-[36px] max-sm:leading-[42px] font-bold lg:max-w-lg">We Provide You Super Quality Shoes</h3>
+          <h3 className="font-palanquin capitalize text-4xl max-sm:text-[36px] max-sm:leading-[42px] font-bold lg:max-w-lg">We Provide You Super Quality Watches</h3>
           <p className="info-text mt-4 lg:max-w-lg">
             Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
           <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
